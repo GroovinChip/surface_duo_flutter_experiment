c14418239fbde25ece66828bc27e2f6257a9dc6a
Reuben Turner <groovinchip@gmail.com>
2020-09-03 13:37:01 -0400

    using multi_screen_layout

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ A Flutter app to experiment with the Surface Duo SDK.
 
 1) Example from the surface_duo Flutter package example
 2) List/details example
+3) Example from the multi_screen_layout Flutter package example
--- lib/main.dart
+++ lib/main.dart
@@ -1,167 +1,120 @@
 import 'package:flutter/material.dart';
-import 'package:surface_duo/surface_duo.dart';
+import 'package:multi_screen_layout/multi_screen_layout.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Surface Duo Test 2'),
+    return TwoPageLayout(
+      child: MainPage(),
+      secondChild: SecondPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class MainPage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Page'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('This page always displays!'),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                ListTile(
+                  title: Text('Surface Duo Device Info'),
+                  trailing: Icon(Icons.chevron_right),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => SurfaceDuoInfoPage(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String _selectedItem = 'No selection yet.';
+class SecondPage extends StatelessWidget {
+  final bool showAppBar;
 
-  /// Surface Duo stuff
-  bool _isDualScreenDevice = false;
-  bool _isAppSpanned = false;
-  double _hingeAngle = 180.0;
-  int _hingeSize = 0;
+  const SecondPage({
+    Key key,
+    this.showAppBar = true,
+  }) : super(key: key);
 
   @override
-  void initState() {
-    super.initState();
-    SurfaceDuo.isDualScreenDevice.then((value) {
-      setState(() {
-        _isDualScreenDevice = value;
-      });
-    });
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: showAppBar
+          ? AppBar(
+              title: Text('Second Page'),
+            )
+          : null,
+      backgroundColor: Colors.tealAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('This page only shows when spanned on 2 screens!'),
+          ],
+        ),
+      ),
+    );
   }
+}
 
-  // Surface Duo: We'll use this simple function to query the APIs and report their values
-  Future<void> _updateDualScreenInfo() async {
-    // print("_updateDualScreenInfo() - Start");
-    _isDualScreenDevice = await SurfaceDuo.isDualScreenDevice;
-    _isAppSpanned = await SurfaceDuo.isAppSpanned;
-    _hingeAngle = await SurfaceDuo.getHingeAngle;
-    _hingeSize = await SurfaceDuo.getHingeSize;
-    // print("_isDualScreenDevice: $_isDualScreenDevice");
-    // print("_isAppSpanned: $_isAppSpanned");
-    // print("_hingeAngle: $_hingeAngle");
-    // print("_hingeSize: $_hingeSize");
-    // print("_updateDualScreenInfo() - End");
-  }
+class SurfaceDuoInfoPage extends StatelessWidget {
+  const SurfaceDuoInfoPage();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Surface Duo Info'),
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return FutureBuilder<void>(
-            future: _updateDualScreenInfo(),
-            builder: (context, snapshot) {
-              return _createPage();
-            },
+      body: MultiScreenInfo(
+        builder: (info) {
+          return TwoPageLayout(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text('The below information is from the Surface Duo SDK'),
+                  Text(
+                      'isDualScreenDevice: ${info.surfaceDuoInfoModel.isDualScreenDevice}'),
+                  Text('isAppSpanned: ${info.surfaceDuoInfoModel.isSpanned}'),
+                  Text('hingeAngle: ${info.surfaceDuoInfoModel.hingeAngle}'),
+                ],
+              ),
+            ),
+            secondChild: SecondPage(
+              showAppBar: false,
+            ),
           );
         },
       ),
-    );
-  }
-
-  Widget _createPage() {
-    if (!_isDualScreenDevice || (_isDualScreenDevice && !_isAppSpanned)) {
-      // We are not on a dual-screen device or
-      // we are but we are not spanned
-      return _buildList();
-    } else {
-      // We are on a dual-screen device and we are spanned
-      if (MediaQuery.of(context).orientation == Orientation.portrait) {
-        // Portrait is what we get when we have rotated the device
-        // and have two "landscape" screens on top of each other,
-        // so together they are "portrait"
-        return Column(
-          children: [
-            Flexible(
-              flex: 1,
-              child: _buildList(),
-            ),
-            SizedBox(height: _hingeSize.toDouble()),
-            Flexible(
-              flex: 1,
-              child: Center(
-                child: _buildDetails(),
-              ),
-            ),
-          ],
-        );
-      } else {
-        return Row(
-          children: [
-            Flexible(
-              flex: 1,
-              child: _buildList(),
-            ),
-            SizedBox(width: _hingeSize.toDouble()),
-            Flexible(
-              flex: 1,
-              child: Center(
-                child: _buildDetails(),
-              ),
-            ),
-          ],
-        );
-      }
-    }
-  }
-
-  Widget _buildList() {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('List item #${index + 1}'),
-          onTap: () {
-            /// Currently, if the user selects an item in single screen mode,
-            /// and then spans the app across both screens, we do not actually
-            /// move into a spanned mode where we have distinct content on each
-            /// screen. todo: figure out how to return to the correct spanned mode.
-            if (!_isDualScreenDevice ||
-                (_isDualScreenDevice && !_isAppSpanned)) {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) {
-                    return Scaffold(
-                      appBar: AppBar(),
-                      body: Center(
-                        child: Text('Item #${index + 1}'),
-                      ),
-                    );
-                  },
-                ),
-              );
-            }
-            setState(() => _selectedItem = 'Item #${index + 1}');
-          },
-        );
-      },
-    );
-  }
-
-  Widget _buildDetails() {
-    return Center(
-      child: Text(_selectedItem),
     );
   }
 }
